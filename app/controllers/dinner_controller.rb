26a677da580cb5209801d43e49516b467f0b24b4
Elise Brown <elisemarie88@gmail.com>
2021-08-28 12:22:59 -0600

    Dinner Delete function working

--- app/controllers/dinner_controller.rb
+++ app/controllers/dinner_controller.rb
@@ -1,26 +1,27 @@
 class DinnerController < ApplicationController
+before_action :set_dinner, only:[:show, :edit, :update, :destroy]  #This exists to call set_dinner (which grabs the specific id) and stores it in @dinner
 
-
-  def index  #This exists to direct to the view of all the dinners
+  def index  #This exists to direct you to the view of all the dinners
     dinners = Dinner.all
     render component: "Dinners", props:{dinners:dinners}
   end
 
-  def new  #This exists to direct to a form for a new dinner
+  def new  #This exists to direct you to a form for a new dinner
     render component: "NewDinner"
   end
 
-  def create  #This exists to add the info from the NewDinner form to the Dinners method array.
+  def create  #This exists to add the info from the NewDinner form to the Dinners method array and takes you back to root.
     @dinner = Dinner.new(dinner_params) # Dinner.new creates a new object to put something in Is it automatically filled with the info from NewDinner form? @dinner exists if the new dinner gets through dinner_params's permissions?
       if (@dinner.save) # If you CAN save it once it passes dinner_params permissions... then...
-        redirect_to dinners_path  
+        redirect_to root_path  
       else
         #TODO w/bad info
       end
   end
 
-  def destroy
-    
+  def destroy  #This takes the specific dinnerID (found from set_dinner) and destroys it and then takes you back to root.
+    @dinner.destroy
+    redirect_to root_path
   end
 
 
